42fb132085fa12d967ffeb73d648a4772691fb52
Антон <laykon10@yandex.ru>
2021-09-04 09:12:57 +0300

    may be

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	var l = -1
-	for i := 0; i < len(os.Args); i++ {
-		if len(os.Args[i]) > 0 {
-			l++
-		}
-	}
+	// var l = -1
+	// for i := 0; i < len(os.Args); i++ {
+	// fmt.Println(os.Args[i])
+	// if len(os.Args[i]) > 0 {
+	// l++
+	// }
+	// }
+	s := os.Args[1]
+	var l = len(strings.Split(s, ""))
 	fmt.Print(l)
-	return
 }
